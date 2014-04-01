74e17e3b118d3501d65b6ac7c99f4e91b9965533
Anthony Ferrara <aferrara@google.com>
2014-04-01 18:27:35 -0400

    Add service error tracking

--- service.sh
+++ service.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-nohup php5 cli.php slave > /tmp/process1.out 2>&1 &
-nohup php5 cli.php slave > /tmp/process2.out 2>&1 &
-nohup php5 cli.php slave > /tmp/process3.out 2>&1 &
-nohup php5 cli.php slave > /tmp/process4.out 2>&1 &
+nohup php5 cli.php slave > /tmp/process1.out 2> /tmp/process1.err &
+nohup php5 cli.php slave > /tmp/process2.out 2> /tmp/process2.err &
 
