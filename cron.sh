9a2ccfa6b90090fb5c1b056246646385685e7134
Anthony Ferrara <aferrara@google.com>
2014-04-02 10:31:32 -0400

    Fixed path issue with cron

--- cron.sh
+++ cron.sh
@@ -2,6 +2,8 @@
 
 PROCS=`pgrep hhvm | wc -l`
 
+cd /parser
+
 while [ `pgrep hhvm | wc -l` -lt "2" ]; do
     echo "Spawning new process"
     nohup hhvm cli.php --max-requests="5" slave > /dev/null 2>&1 &
