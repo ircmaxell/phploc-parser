f90eb8c0a0beefcb76c4937f00480eed3435e5a2
Anthony Ferrara <aferrara@google.com>
2014-04-02 10:28:34 -0400

    Add sleep to prevent over spawning

--- cron.sh
+++ cron.sh
@@ -5,4 +5,5 @@ PROCS=`pgrep hhvm | wc -l`
 while [ `pgrep hhvm | wc -l` -lt "2" ]; do
     echo "Spawning new process"
     nohup hhvm cli.php --max-requests="5" slave > /dev/null 2>&1 &
+    sleep 1
 done
